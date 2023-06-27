638f2b5f8af61ccfe0a983304aa1da209f2b6e15
KaiSteffens <kaiane-tsadrosa@educar.rs.gov.br>
2023-06-27 19:49:35 -0300

    Atividade02 JPA

--- exercicio01_jpa/Entity/Usuario.java
+++ exercicio01_jpa/Entity/Usuario.java
@@ -1,6 +1,7 @@
 package com.example.exercicio01_jpa.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,6 +15,7 @@ import org.hibernate.annotations.ManyToAny;
 @Entity
 
 public class Usuario {
+    @Id
     private Long id;
     private String nome;
 
